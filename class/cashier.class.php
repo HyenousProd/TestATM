133451f717b715fdc23e4b2dd453b87d020839db
Quentin <q.fetiveau@gmail.com>
2020-06-09 23:49:00 +0200

    TestATM first iteration class + test page

---
+++ class/cashier.class.php
@@ -0,0 +1,102 @@
+<?php 
+class cashier
+{
+
+    private $_cash; // All available change in cashier (array)
+    
+    public function __construct()
+    {
+        $this->_cash = array ( 1 => 25, 2 => 74, 5 => 14, 10 => 18, 20 => 0, 50 => 5, 100 => 30, 200 => 15, 500 => 8, 1000 => 11, 2000 => 8, 5000 => 5, 10000 => 2, 20000 => 0, 50000 => 0 );
+;
+    }
+
+    // GETTER --------
+    private function getCashNb($changeVal) // Get available change for a specific amount
+    {
+        $p = $this->getAvailableChange();
+        return $p[$changeVal];
+    }   
+
+    public function getAvailableChange() // Get all available change (array)
+    {
+        return $this->_cash;
+    }
+    
+    public function getChangeToGiveBack($cost, $paid) // Compute total amount of money to give back to customer
+    {
+        if($paid >= $cost) // Return amount if correct
+        {
+            return($paid - $cost);   
+        }
+        else
+        {
+            return false; //Return error if customer paid less than the bill
+        }
+    }
+    
+    public function getCustomerChange($cost, $paid) // get all change to give back to the customer
+    {
+        $giveBack = $this->getChangeToGiveBack($cost, $paid)*100; // Compute total amount to give back & convert to cts
+        $changeToGive = array();
+        $temp_array = array_reverse($this->getAvailableChange(), true); // Reorder array to browse from higher amount to lower
+        
+        foreach($temp_array as $changeVal => $changeNb) // Browse available change
+        {
+            if($changeVal <= $giveBack and $this->getCashNb($changeVal) > 0)  // Check if this amount is relevant and if still available
+            {
+                $partInt = intval($giveBack/$changeVal); // Compute the max nb of items we can give back for this amount
+                if($partInt <= $this->getCashNb($changeVal)) // If max quantity available, give everything required for this amount
+                {
+                    $giveBack -= $partInt*$changeVal;
+                    $changeToGive[$changeVal] = $partInt;
+                    $this->removeCash($changeVal, $partInt); // Update available change
+                }
+                elseif($partInt > $this->getCashNb($changeVal)) // If not enough quantity for this amount, give all we have in stock
+                {
+                    $giveBack -= $this->getCashNb($changeVal)*$changeVal;
+                    $changeToGive[$changeVal] = $this->getCashNb($changeVal);
+                    $this->removeCash($changeVal, $this->getCashNb($changeVal)); // Update available change
+                }
+                if($giveBack == 0) // Once we have enough to give the money back, break the loop
+                {
+                    break;
+                }
+            }
+        }
+        if($giveBack == 0) // If we could give back all the money required, proceed and return array
+        {
+            return $changeToGive;
+        }
+        else // if we had not enough change available, return error and put the money back
+        {
+            foreach($changeToGive as $val => $nb)
+            {
+                $this->addCash($val, $nb);
+            }
+            return false;
+        }   
+    }
+
+    //SETTER ---------
+    
+    private function removeCash($val, $nb) // Remove cash from available change
+    {
+        //Check if we do not try to take more money than available
+        if(($this->getCashNb($val) - $nb) >=0)
+        {
+            $this->_cash[$val] -= $nb; 
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }  
+    
+    private function addCash($val, $nb) // Add cash to available change
+    {
+        $this->_cash[$val] += $nb; 
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+require_once('class/cashier.class.php');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test ATM</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <?php
+            $cash = new cashier();
+            //Display default change availability
+            echo "<h2>Available change</h2>"; 
+            echo "<ul>";
+                foreach($cash->getAvailableChange() as $key=>$content)
+                {
+                    if($key >=100)
+                    {
+                        echo "<li>".$content." x ".($key/100)."€</li>";  
+                    }
+                    else
+                    {
+                        echo "<li>".$content." x ".$key."cts</li>";     
+                    }  
+                }
+            echo "</ul>";
+        
+            //Check form 
+            if(isset($_POST['cost']) and $_POST['cost'] <= $_POST['paid'])
+            {
+                echo "<h2>You must give back ".$cash->getChangeToGiveBack($_POST['cost'], $_POST['paid'])."€</h2>";
+                $giveBack = $cash->getCustomerChange($_POST['cost'], $_POST['paid']);
+                if($giveBack == false)
+                {
+                    echo "<h3>Error : you do not have enough change</h3>";    
+                }
+                else
+                {
+                    echo "<ul>";
+                    foreach($giveBack as $key=>$content)
+                    {
+                        if($key >=100)
+                        {
+                            echo "<li>".$content." x ".($key/100)."€</li>";  
+                        }
+                        else
+                        {
+                            echo "<li>".$content." x ".$key."cts</li>";     
+                        }
+                    }
+                    echo "</ul>";
+
+                    echo "<h2>Remaining change after payback</h2>";
+                    echo "<ul>";
+                    foreach($cash->getAvailableChange() as $key=>$content)
+                    {
+                        if($key >=100)
+                        {
+                            echo "<li>".$content." x ".($key/100)."€</li>";  
+                        }
+                        else
+                        {
+                            echo "<li>".$content." x ".$key."cts</li>";     
+                        }  
+                    }
+                    echo "</ul>";    
+                }     
+            }
+            elseif(isset($_POST['cost']) and $_POST['cost'] > $_POST['paid'])
+            {
+                echo "<h2>Error : customer did not pay enough !</h2>";    
+            }
+            else
+            {
+                echo "<h2>Enter values below</h2>";
+            }
+        ?>
+        <form method="post" action="index.php">
+            Bill <input type="text" name="cost" />
+            Paid <input type="text" name="paid" />
+            <input type="submit" name="Send" />
+        </form>
+    </body>
+</html>
+
